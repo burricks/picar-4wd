4e0ba59f4f45837f76dd591b929d88f39df11f00
burricks <lisasdina@gmail.com>
2022-03-08 15:51:48 -0500

    two data points

--- lab2/lab2_utils.py
+++ lab2/lab2_utils.py
@@ -1,4 +1,29 @@
 import picar_4wd as fc
+import json
+
+fc.start_speed_thread()
+
+
+class Stats():
+    def __init__(self, speed, distanceAhead):
+        self.speed = speed
+        self.distanceAhead = distanceAhead
+
+    def toDict(self):
+        return json.dumps({"speed": self.speed, "distanceAhead": self.distanceAhead})
+
+
+def distanceAhead():
+    aheadDistance = fc.get_distance_at(0)
+    if aheadDistance == -2 or aheadDistance > 100:
+        return ">100"
+    return str(aheadDistance)
+
+def speed():
+    return fc.speed_val()
+
+def getStats():
+    return Stats(speed(), distanceAhead()).toDict()
 
 def isAnythingAhead():
     aheadDistance = fc.get_distance_at(0)
@@ -13,10 +38,10 @@ def goBackwards():
     fc.backward(20)
 
 def goRight():
-    fc.goRight(20)
+    fc.turn_right(20)
 
 def goLeft():
-    fc.goLeft(20)
+    fc.turn_left(20)
 
 def stop():
     fc.stop()
--- lab2/wifi_server.py
+++ lab2/wifi_server.py
@@ -1,4 +1,5 @@
 import socket
+import lab2_utils
 #import picar_4wd as fc
 
 HOST = "192.168.4.131" # IP address of your Raspberry PI
@@ -11,28 +12,29 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
     try:
         while 1:
+            carstats = lab2_utils.getStats()
+            client.send(carstats.encode('utf-8')) # Echo back to client
             data = client.recv(1024) 
             stringdata = data.decode('utf-8')
             if data:
                 if stringdata == "38":
                     print("moving forward")
-                    # lab2_utils.goForward()
+                    lab2_utils.goForward()
                 elif stringdata == "40":
                     print("moving backwards")
-                    #lab2_utils.goBackwards()
+                    lab2_utils.goBackwards()
                 elif stringdata == "37":
                     print("going left")
-                    #lab2_utils.goLeft()
+                    lab2_utils.goLeft()
                 elif stringdata == "39":
                     print("going right")
-                    #lab2_utils.goRight()
+                    lab2_utils.goRight()
                 elif stringdata == "0":
                     print("stopping")
-                    #lab2_utils.stop()
+                    lab2_utils.stop()
                 else:
                     print("invalid")
                     print(data)
-                    client.send(data) # Echo back to client
     except: 
         print("Closing socket")
         client.close()
